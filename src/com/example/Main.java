52a77a40ceea56768ffed9beda43f8f34cb60429
Feerdus95 <ferdus95.fv@gmail.com>
2023-02-09 14:22:07 -0300

    Typo en comentarios

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String[] array = {"Cuando","tuve","yo","te","tuve,","te","mantuve","y","te","di"};  //Un array de Strings.
         String result = "";                                                                 //Array vacío.
-        for( String item : array){          //Buche forEach, que pone cada elemento del 1º array en el vacío
+        for( String item : array){          //Bucle forEach, que pone cada elemento del 1º array en el vacío
             result += item + " ";           //junto con un espacio.
         }
         System.out.println(result);
